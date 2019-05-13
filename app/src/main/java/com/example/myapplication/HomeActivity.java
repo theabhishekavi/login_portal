6980b6f6fbe724181d6a3cb4dd49d0580defe847
theabhishekavi <abhishekavi4602@gmail.com>
2019-05-13 10:08:19 +0530

    Area pressed and device orientation done

--- app/src/main/java/com/example/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -14,20 +14,16 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Display;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.GridLayout;
 import android.widget.Toast;
 
-public class HomeActivity extends AppCompatActivity implements SensorEventListener {
+public class HomeActivity extends AppCompatActivity {
 
     private TabAdapter adapter;
     private TabLayout tabLayout;
     private ViewPager viewPager;
-
-    private SensorManager mSensorManager;
-    private Sensor mRotationSensor;
-
-    private static final int SENSOR_DELAY = 500 * 1000; // 500ms
-    private static final int FROM_RADS_TO_DEGS = -57;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -41,53 +37,6 @@ public class HomeActivity extends AppCompatActivity implements SensorEventListen
         adapter.addFragment(new LoginFragment(), "Password");
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
-
-        try {
-            mSensorManager = (SensorManager) getSystemService(Activity.SENSOR_SERVICE);
-            mRotationSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
-            mSensorManager.registerListener(this, mRotationSensor, SENSOR_DELAY);
-        } catch (Exception e) {
-            Toast.makeText(this, "Hardware compatibility issue", Toast.LENGTH_LONG).show();
-        }
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        if (event.sensor == mRotationSensor) {
-            if (event.values.length > 4) {
-                float[] truncatedRotationVector = new float[4];
-                System.arraycopy(event.values, 0, truncatedRotationVector, 0, 4);
-                update(truncatedRotationVector);
-            } else {
-                update(event.values);
-            }
-        }
-    }
-    private void update(float[] vectors) {
-        float[] rotationMatrix = new float[9];
-        SensorManager.getRotationMatrixFromVector(rotationMatrix, vectors);
-        int worldAxisX = SensorManager.AXIS_X;
-        int worldAxisZ = SensorManager.AXIS_Z;
-        float[] adjustedRotationMatrix = new float[9];
-        SensorManager.remapCoordinateSystem(rotationMatrix, worldAxisX, worldAxisZ, adjustedRotationMatrix);
-        float[] orientation = new float[3];
-        SensorManager.getOrientation(adjustedRotationMatrix, orientation);
-        float pitch = orientation[1] * FROM_RADS_TO_DEGS;
-        float roll = orientation[2] * FROM_RADS_TO_DEGS;
-        Log.e("pitchh","Pitch: "+(int)(pitch*2));
-//        if (Math.abs((int)(pitch*2))<=50)
-//        Toast.makeText(this,"Upright",Toast.LENGTH_SHORT).show();
-//        if (Math.abs((int)(pitch*2))>50 && Math.abs((int)(pitch*2))<150)
-//            Toast.makeText(this,"Tilt",Toast.LENGTH_SHORT).show();
-//        if (Math.abs((int)(pitch*2))>=150)
-//            Toast.makeText(this,"Rest",Toast.LENGTH_SHORT).show();
-//        Log.e("pitchhhhh","Roll is "+(int)roll);
-//        ((TextView)findViewById(R.id.pitch)).setText("Pitch: "+pitch);
-//        ((TextView)findViewById(R.id.roll)).setText("Roll: "+roll);
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
 }
--- app/src/main/java/com/example/myapplication/mPinFragment.java
+++ app/src/main/java/com/example/myapplication/mPinFragment.java
@@ -1,8 +1,13 @@
 package com.example.myapplication;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Paint;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.annotation.NonNull;
@@ -16,13 +21,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class mPinFragment extends Fragment {
+public class mPinFragment extends Fragment implements SensorEventListener {
     int counter = 0;
     long startTime = SystemClock.elapsedRealtime();
-    TextView timelapse,cordinates;
+    TextView timelapse,cordinates,area,tvorientation;
+
+
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    float orientationX,orientationY,orientationZ;
+//    private static final int SENSOR_DELAY = 500 * 1000; // 500ms
+//    private static final int FROM_RADS_TO_DEGS = -57;
+//    private float pitch;
 
     @Nullable
     @Override
@@ -47,8 +61,10 @@ public class mPinFragment extends Fragment {
         final Button btnenter = getView().findViewById(R.id.btnenter);
         final TextView tvNewUser = getView().findViewById(R.id.tvNewUser);
 
+        area = getView().findViewById(R.id.area);
         timelapse = getView().findViewById(R.id.timelapse);
         cordinates = getView().findViewById(R.id.cordinates);
+        tvorientation = getView().findViewById(R.id.orientation);
         tvNewUser.setPaintFlags(tvNewUser.getPaintFlags()| Paint.UNDERLINE_TEXT_FLAG);
 
         tvNewUser.setOnClickListener(new View.OnClickListener() {
@@ -57,6 +73,14 @@ public class mPinFragment extends Fragment {
                 startActivity(new Intent(getContext(), RegisterActivity.class));
             }
         });
+
+        try {
+            mSensorManager = (SensorManager) getContext().getSystemService(Activity.SENSOR_SERVICE);
+            mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        } catch (Exception e) {
+            Toast.makeText(getContext(), "Hardware compatibility issue", Toast.LENGTH_LONG).show();
+        }
 
 //        btn1.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -154,10 +178,18 @@ public class mPinFragment extends Fragment {
             if (event.getAction() == MotionEvent.ACTION_DOWN) {
                 int x = (int)event.getRawX();
                 int y = (int)event.getRawY();
-//                int k = getResources().getConfiguration().orientation;
-//                int l =((WindowManager) getContext().getSystemService(getContext().WINDOW_SERVICE)).getDefaultDisplay().getOrientation();
-//               Toast.makeText(getContext(),"size is "+event.getSize(0),
-//                       Toast.LENGTH_SHORT).show();
+
+//                if (Math.abs((int)(pitch*2))<=50)
+//                    Toast.makeText(getContext(),"Upright",Toast.LENGTH_SHORT).show();
+//                if (Math.abs((int)(pitch*2))>50 && Math.abs((int)(pitch*2))<150)
+//                    Toast.makeText(getContext(),"Tilt",Toast.LENGTH_SHORT).show();
+//                if (Math.abs((int)(pitch*2))>=150)
+//                    Toast.makeText(getContext(),"Rest",Toast.LENGTH_SHORT).show();
+//
+                tvorientation.setText("Orientation x : "+orientationX+" y : "+orientationY
+                +" z : "+orientationZ);
+                area.setText("size is "+event.getSize(0));
+
                 cordinates.setText("Cordinates are x:"+x+" and y:"+y);
                 long difference = (SystemClock.elapsedRealtime()-startTime);
                 if(counter == 0)
@@ -170,4 +202,42 @@ public class mPinFragment extends Fragment {
             return true;
         }
     };
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        orientationX = event.values[0];
+        orientationY = event.values[1];
+        orientationZ = event.values[2];
+
+//        Log.e("motionn", "x:"+event.values[0]+" y:"+event.values[1]+"z: "+
+//                event.values[2]);
+//        if (event.sensor == mRotationSensor) {
+//            if (event.values.length > 4) {
+//                float[] truncatedRotationVector = new float[4];
+//                System.arraycopy(event.values, 0, truncatedRotationVector, 0, 4);
+//                update(truncatedRotationVector);
+//            } else {
+//                update(event.values);
+//            }
+//        }
+    }
+
+//    private void update(float[] vectors) {
+//        float[] rotationMatrix = new float[9];
+//        SensorManager.getRotationMatrixFromVector(rotationMatrix, vectors);
+//        int worldAxisX = SensorManager.AXIS_X;
+//        int worldAxisZ = SensorManager.AXIS_Z;
+//        float[] adjustedRotationMatrix = new float[9];
+//        SensorManager.remapCoordinateSystem(rotationMatrix, worldAxisX, worldAxisZ, adjustedRotationMatrix);
+//        float[] orientation = new float[3];
+//        SensorManager.getOrientation(adjustedRotationMatrix, orientation);
+//        pitch = orientation[1] * FROM_RADS_TO_DEGS;
+//        float roll = orientation[2] * FROM_RADS_TO_DEGS;
+//        Log.e("pitchh","Pitch: "+(int)(pitch*2));
+//    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
 }
