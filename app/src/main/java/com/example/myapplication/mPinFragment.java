fe603632dddf32b8111279cbec60fd01d45a1b65
theabhishekavi <abhishekavi4602@gmail.com>
2019-05-13 15:28:04 +0530

    device parameters stored

--- app/src/main/java/com/example/myapplication/mPinFragment.java
+++ app/src/main/java/com/example/myapplication/mPinFragment.java
@@ -1,18 +1,27 @@
 package com.example.myapplication;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Paint;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.net.wifi.SupplicantState;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.Display;
 import android.view.LayoutInflater;
@@ -25,23 +34,25 @@ import android.widget.GridLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.lang.reflect.Method;
+
 public class mPinFragment extends Fragment implements SensorEventListener {
     int counter = 0;
     long startTime = SystemClock.elapsedRealtime();
-    TextView timelapse,cordinates,area,tvorientation;
+    TextView timelapse, cordinates, area, tvorientation, deviceDetail,tvImei;
 
 
     private SensorManager mSensorManager;
     private Sensor mAccelerometer;
-    float orientationX,orientationY,orientationZ;
-//    private static final int SENSOR_DELAY = 500 * 1000; // 500ms
-//    private static final int FROM_RADS_TO_DEGS = -57;
-//    private float pitch;
+    private float orientationX, orientationY, orientationZ;
+
+    TelephonyManager telephonyManager;
+    private String IMEI, IMSI, SimNo;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_mpin,container,false);
+        return inflater.inflate(R.layout.activity_mpin, container, false);
     }
 
     @Override
@@ -65,7 +76,10 @@ public class mPinFragment extends Fragment implements SensorEventListener {
         timelapse = getView().findViewById(R.id.timelapse);
         cordinates = getView().findViewById(R.id.cordinates);
         tvorientation = getView().findViewById(R.id.orientation);
-        tvNewUser.setPaintFlags(tvNewUser.getPaintFlags()| Paint.UNDERLINE_TEXT_FLAG);
+        deviceDetail = getView().findViewById(R.id.deviceDetail);
+        tvImei = getView().findViewById(R.id.tvimei);
+
+        tvNewUser.setPaintFlags(tvNewUser.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
 
         tvNewUser.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,10 +88,84 @@ public class mPinFragment extends Fragment implements SensorEventListener {
             }
         });
 
+//        String imsi = android.os.SystemProperties.get(android.telephony.TelephonyPropertie.PROPERTY_IMSI);
+
+
+        WifiManager wifiManager = (WifiManager) getContext().getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+        String macAddress = wifiInfo.getMacAddress();
+        String wifiid = "";
+        if (wifiInfo.getSupplicantState() == SupplicantState.COMPLETED) {
+            wifiid = wifiInfo.getSSID();
+        }
+        Log.e("addresssss", "mac is " + macAddress + " wifi id is " + wifiid);
+
+        String model = Build.MODEL;
+        Log.e("modelll", "model name is " + model);
+
+
+        telephonyManager = (TelephonyManager) getContext()
+                .getSystemService(Context.TELEPHONY_SERVICE);
+        String operatorName = telephonyManager.getNetworkOperatorName();
+
+        Log.e("operator ", "operator name is " + operatorName);
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+           ActivityCompat.requestPermissions(getActivity(),
+                   new String[]{Manifest.permission.READ_PHONE_STATE},
+                   123);
+
+        }
+        int permission = ContextCompat.checkSelfPermission(getContext(),
+                Manifest.permission.READ_PHONE_STATE);
+        if(permission == PackageManager.PERMISSION_GRANTED){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                IMEI = telephonyManager.getImei();
+                SimNo = telephonyManager.getSimSerialNumber();
+                IMSI = telephonyManager.getSubscriberId();
+            }
+            else{
+                IMEI = telephonyManager.getDeviceId();
+                SimNo = telephonyManager.getSimSerialNumber();
+                IMSI = telephonyManager.getSubscriberId();
+
+            }
+            tvImei.setText("IMEI number is "+IMEI + " \n SimNo is "+SimNo +
+                    "\n IMSI is"+IMSI);
+        }
+
+        deviceDetail.setText(" Mac Address is "+macAddress+"\n Model number is "
+        +model+"\n Operator name is "+operatorName);
+
+        //not done sections
+
+//        telephonyManager.getSubscriberId();
+//        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+////            IMEI = telephonyManager.getDeviceId();
+//            return;
+//        }
+
+
         try {
-            mSensorManager = (SensorManager) getContext().getSystemService(Activity.SENSOR_SERVICE);
-            mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-            mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+            mSensorManager = (SensorManager) getContext()
+                    .getSystemService(Activity.SENSOR_SERVICE);
+            mAccelerometer = mSensorManager
+                    .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            mSensorManager
+                    .registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
         } catch (Exception e) {
             Toast.makeText(getContext(), "Hardware compatibility issue", Toast.LENGTH_LONG).show();
         }
@@ -172,6 +260,39 @@ public class mPinFragment extends Fragment implements SensorEventListener {
 
 
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+//    public static String get(Context context, String key) {
+//        String ret = "";
+//
+//        try {
+//            ClassLoader cl = context.getClassLoader();
+//            @SuppressWarnings("rawtypes")
+//            Class SystemProperties = cl.loadClass("android.os.SystemProperties");
+//
+//            //Parameters Types
+//            @SuppressWarnings("rawtypes")
+//            Class[] paramTypes= new Class[1];
+//            paramTypes[0]= String.class;
+//
+//            Method get = SystemProperties.getMethod("get", paramTypes);
+//
+//            //Parameters
+//            Object[] params = new Object[1];
+//            params[0] = new String(key);
+//
+//            ret = (String) get.invoke(SystemProperties, params);
+//        } catch(Exception e) {
+//            ret = "";
+//            //TODO : Error handling
+//        }
+//
+//        return ret;
+//    }
     View.OnTouchListener handleTouch = new View.OnTouchListener() {
         @Override
         public boolean onTouch(View v, MotionEvent event) {
